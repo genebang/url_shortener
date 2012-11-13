1e9a7c7f8c92925a531dfb2635b7c63aec3c5958
Gene Bang <genebang@gmail.com>
2012-11-12 21:53:37 -0800

    start url_shortener project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortner::Application.config.secret_token = '0f2107313de42689edf87d97f54a0284ebec9af94bad98c9b7b820de81ea450ea45f9b49e9dfbd3cff1cfbaf8fdb54f57936f78f25e4b577c37ac03c2dc9f727'
